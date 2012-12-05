ff130fe2033b7a28fba7b045f99c2c574d6d30de
n2o <cmeter@googlemail.com>
2012-12-05 17:08:08 +0100

    Enough for today

--- Compiler.java
+++ Compiler.java
@@ -5,44 +5,25 @@ import java.io.*;
 
 public class Compiler {
 
-	public static void main(String args[]) {
-		try {
-			String filename = args[0];
-			File file = new File(filename);
-			PushbackReader reader = new PushbackReader(new FileReader(file)); 
-			Lexer lexer = new Lexer(reader); 
-			Parser parser = new Parser(lexer); 
-			Start tree = parser.parse();
-
-			// CodeGen visitor = new CodeGen();
-			// tree.apply(visitor); 
-			
-			String output =
-		    	".bytecode 50.0\n"+
-				".class public Out\n"+
-				".super java/lang/Object\n"+
-				".method public <init>()V\n"+
-				"\t.limit stack 1\n"+
-				"\t.limit locals 1\n"+
-				"\taload_0\n"+
-				"\tinvokespecial java/lang/Object/<init>()V\n"+
-				"\treturn\n"+
-				".end method\n"+
-				"\t.method public static main([Ljava/lang/String;)V\n"+
-				"\t.limit stack 10\n"+
-				"\t.limit locals "+visitor.countIdentifier+"\n"+
-			 	visitor.code+ 
-				"\treturn\n"+
-				".end method\n";
-			// System.out.println(output);
-
-			Writer wout = new BufferedWriter(
-			new OutputStreamWriter(
-			new FileOutputStream("Out.j"),"UTF8"));
-			wout.append(output);
-			wout.close();
-		} catch(Exception e) {
-			System.out.println("Aaaaah irgendein Fehler ist aufgetreten!");
-		}
+	public static void main(String[] args) throws LexerException, IOException, ParserException {
+		System.out.println("############################################################################");
+		String input =  "program HelloWorld; \n";
+			   input += "begin \n";
+			   input += "writeln('HelloWorld') \n";
+			   input += "end. \n";
+		System.out.println("# Eingabe:  \n"+input);
+		parse(input);
+		System.out.println("############################################################################");
 	}
+
+	private static void parse(String input) throws ParserException, LexerException, IOException {
+		StringReader reader = new StringReader(input);
+		PushbackReader r = new PushbackReader(reader, 100);
+		Lexer l = new Lexer(r);
+		Parser parser = new Parser(l);
+		Start start = parser.parse();
+		ASTPrinter printer = new ASTPrinter();
+		start.apply(printer);
+	}
+
 }
