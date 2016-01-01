d2e65c559157b066f35f6d6f661c83428273e34d
n2o <christian.meter@uni-duesseldorf.de>
2016-01-01 19:20:18 +0100

    Tidy up

--- out/production/projekt/testCases/4.pas
+++
@@ -1,6 +0,0 @@
-program initiation;
-var a : integer;
-begin
-	a := 3;
-	writeln(a);
-end  .
--- out/production/projekt/tests/10.pas
+++
@@ -1,7 +0,0 @@
-program not_john;
-var fi_lo: boolean ; 
-var c, b, a : integer;
-begin
-	if (2=2) and not(2<>2) then writeln ( 1 );
-
-end .
